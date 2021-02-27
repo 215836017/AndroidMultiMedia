2871a986b9abb37baadd5368c51be6ea55593c13
215836017 <215836017@qq.com>
2021-02-27 21:35:53 +0800

    添加DemoMediaExtractor：第一次提交

---
+++ DemoMediaExtractor/app/src/main/java/com/cakes/demomediaextractor/MainActivity.java
@@ -0,0 +1,18 @@
+package com.cakes.demomediaextractor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * https://www.cnblogs.com/guanxinjing/p/11378133.html
+ * https://www.jianshu.com/p/14daab91b951
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
